7e495fd83c8cbb78d966a228afad13b4db36b553
JoshLuong <joshua.luong@sap.com>
2022-12-17 13:15:36 -0800

    fix: colors

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -13,52 +13,52 @@ const LandingPage = () => {
       <div className="landing-page__container-sections">
         <LandingPageSection
           text="Studio"
-          backgroundColor="#e91e63"
-          textColor="black"
+          backgroundColor="#00522a"
+          textColor="#7ddf64"
           onClick={() => navigate("/studio")}
         />
         <LandingPageSection
           text="In Studio: Lucy"
-          backgroundColor="#fdc500"
-          textColor="black"
+          backgroundColor="#212121"
+          textColor="#ff7f11"
           width="8em"
           fontFamily="'Bodoni Moda', serif"
           onClick={() => navigate("/lucy")}
         />
         <LandingPageSection
           text="Cassie Wolde"
-          backgroundColor="#06D6A0"
-          textColor="black"
+          backgroundColor="#990000"
+          textColor="#f92a82"
         />
         <LandingPageSection
           text="Lowtide Properties: Monarca"
-          backgroundColor="#560bad"
-          textColor="black"
+          backgroundColor="#f7f3e3"
+          textColor="#6f58c9"
           fontSize={medFontSize}
         />
         <LandingPageSection
           text="For The Love of Life"
           fontSize={medFontSize}
-          backgroundColor="#B5179E"
-          textColor="black"
+          backgroundColor="#0a2463"
+          textColor="#00a5cf"
         />
         <LandingPageSection
           text="Granville Island Brewing"
-          backgroundColor="#073B4C"
-          textColor="black"
+          backgroundColor="#00522a"
+          textColor="#7ddf64"
           fontSize={medFontSize}
         />
         <LandingPageSection
           text="Kold"
-          backgroundColor="#4CC9F0"
           width="8em"
           fontFamily="'Bodoni Moda', serif"
-          textColor="black"
+          backgroundColor="#212121"
+          textColor="#ff7f11"
         />
         <LandingPageSection
           text="Sundays Furniture"
-          backgroundColor="#F72585"
-          textColor="black"
+          backgroundColor="#f7f3e3"
+          textColor="#6f58c9"
           fontSize={medFontSize}
         />
       </div>
