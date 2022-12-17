12db0f36b7ea491f5b1662a04d6220233ead9c35
JoshLuong <joshua.luong@sap.com>
2022-12-17 01:48:35 -0800

    fix: spacing horizontal

--- src/components/LandingPageSection.tsx
+++ src/components/LandingPageSection.tsx
@@ -10,12 +10,9 @@ interface LandingPageSectionProps {
 const LandingPageSection: React.FC<LandingPageSectionProps> = ({text, backgroundColor, textColor, fontSize = "6em", onClick}: LandingPageSectionProps) => {
     return (
       <div title={text} className="landing-page-section__container" style={{ backgroundColor, color: textColor, fontSize }} onClick={onClick}>
-        <div>
-            <div className="landing-page-section__text">
-                <span>{text}</span>
-            </div> 
-
-        </div>
+        <div className="landing-page-section__text">
+            <span>{text}</span>
+        </div> 
       </div>
     );
   };
